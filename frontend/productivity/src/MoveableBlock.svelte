5ab185c6a556fd0fcbeea3e033f63ec2045ef3f0
rainerknabenbauer <rainer.knabenbauer@posteo.de>
2021-01-03 17:58:38 +0100

    Move priority into title color

--- frontend/productivity/src/MoveableBlock.svelte
+++ frontend/productivity/src/MoveableBlock.svelte
@@ -19,27 +19,44 @@
         if (isActive) {
             var block = document.getElementById(task.id);
             block.style.position = "absolute";
-            block.style.left = event.clientX-25+"px";
-            block.style.top = event.clientY-25+"px";
+            block.style.left = event.clientX-(block.offsetWidth/2)+"px";
+            block.style.top = event.clientY-(block.offsetHeight/2)+"px";
         }
     });
 </script>
 
 <style>
-    div {
+    .task {
         position: absolute;
         top: 15%;
-        width: 50px;
-        height: 50px;
         cursor: -webkit-grab; 
         cursor: grab;
-        border-bottom-width: 1px;
-        border-color: black;
+        border: 1px solid darkgreen;
+        min-width: 250px;
+        max-width: 500px;
+        background-color: #efefef;
+        border-radius: 3px;
     }
+
+    .title {
+        padding: 5px;
+        background-image: linear-gradient(red, #efefef);
+    }
+
+    .shortDescription {
+        margin: 5px;
+    }
+
 </style>
 
 <!-- The block gets attached to the mouse when you hold down left mouse button -->
-<div id={task.id} on:mousedown={toggle} on:mouseup={toggle}>
-    {task.title}
+<div class="task" id={task.id} on:mousedown={toggle} on:mouseup={toggle}>
+    <div class="header">
+		<div class="details">
+			<div class="title">{task.title}</div>
+			<div class="shortDescription">{task.description.shortDescription}</div>
+		</div>
+    </div>
 </div>
+
 
