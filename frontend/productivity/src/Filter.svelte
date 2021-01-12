8b0febc8d18e743b191452bc2881d6a1a9b5d737
rainerknabenbauer <rainer.knabenbauer@posteo.de>
2021-01-12 22:48:10 +0100

    Remove caption from trash bin

--- frontend/productivity/src/Filter.svelte
+++ frontend/productivity/src/Filter.svelte
@@ -16,7 +16,7 @@
 </style>
 
 
-    <button class="w3-button w3-white w3-hide-small filter w3-margin-right"><i class="fa fa-trash w3-margin-right"></i>Trashbin</button>
+    <button class="w3-button w3-white w3-hide-small filter w3-margin-right"><i class="fa fa-trash"></i></button>
     <button class="w3-button w3-white w3-hide-small filter"><i class="fa fa-map-pin w3-margin-right"></i>Recommended tasks</button>
     <button class="w3-button w3-white filter"><i class="fa fa-diamond w3-margin-right"></i>History</button>
     <button class="w3-button filter">ALL</button>
