b8e05c671931e2355567d0c5cd2b9c1db08ca636
rainerknabenbauer <rainer.knabenbauer@posteo.de>
2021-01-12 23:47:27 +0100

    Update Filter icons

--- frontend/productivity/src/Filter.svelte
+++ frontend/productivity/src/Filter.svelte
@@ -16,7 +16,7 @@
 </style>
 
 
-    <button class="w3-button w3-white w3-hide-small filter w3-margin-right"><i class="fa fa-trash"></i></button>
-    <button class="w3-button w3-white w3-hide-small filter"><i class="fa fa-map-pin w3-margin-right"></i>Recommended tasks</button>
-    <button class="w3-button w3-white filter"><i class="fa fa-diamond w3-margin-right"></i>History</button>
+    <button class="w3-button w3-white w3-hide-small filter w3-margin-right"><i class="fa fa-trash-o"></i></button>
+    <button class="w3-button w3-white w3-hide-small filter"><i class="fa fa-odnoklassniki w3-margin-right"></i>Recommendations</button>
+    <button class="w3-button w3-white filter"><i class="fa fa-heartbeat w3-margin-right"></i>History</button>
     <button class="w3-button filter">ALL</button>
