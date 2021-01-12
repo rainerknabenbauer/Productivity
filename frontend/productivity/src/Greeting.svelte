fbf6e27cec5d7bbb8ff523ec7597288037f1322b
rainerknabenbauer <rainer.knabenbauer@posteo.de>
2021-01-12 22:26:12 +0100

    Update greeting

--- frontend/productivity/src/Greeting.svelte
+++ frontend/productivity/src/Greeting.svelte
@@ -3,15 +3,15 @@
 </script>
 
 <style>
-.lowerleft
-{
-    margin-bottom: 7px;
-    margin-left : 4px;
-    position: fixed;
-    bottom: 0;
-    font-size: 8pt;
-    opacity: 0.3;
-}
+    .lowerleft
+    {
+        margin-bottom: 7px;
+        margin-left : 0px;
+        position: fixed;
+        bottom: 0;
+        font-size: 8pt;
+        opacity: 0.5;
+    }
 </style>
 
-<div class="lowerleft">You're logged in as nykon.</div>
+<div class="lowerleft w3-cursive">Nykon.</div>
