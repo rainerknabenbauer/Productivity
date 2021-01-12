7e15db58d5838bfd43fe18062399cb7d71ebe3e8
rainerknabenbauer <rainer.knabenbauer@posteo.de>
2021-01-12 03:15:38 +0100

    Minor updates

--- frontend/productivity/src/Filter.svelte
+++ frontend/productivity/src/Filter.svelte
@@ -5,14 +5,17 @@
 <style>
     .filter {
 		float: right;
-        color: #636363;
-        background-color: lightgrey;
+        color: lightgrey;
+        background-color: #3a292f;
         opacity: 0.3;
 	}
+    .active {
+        background-color: #25191e;
+    }
 </style>
 
 
-    <button class="w3-button w3-white w3-hide-small filter w3-margin-right"><i class="fa fa-map-pin w3-margin-right"></i>Art</button>
-    <button class="w3-button w3-white w3-hide-small filter"><i class="fa fa-photo w3-margin-right"></i>Photos</button>
-    <button class="w3-button w3-white filter"><i class="fa fa-diamond w3-margin-right"></i>Design</button>
-    <button class="w3-button filter">ALL</button>
+    <button class="w3-button w3-hide-small filter w3-margin-right"><i class="fa fa-map-pin w3-margin-right"></i>Art</button>
+    <button class="w3-button w3-hide-small filter w3-margin-right"><i class="fa fa-photo w3-margin-right"></i>Photos</button>
+    <button class="w3-button filter w3-margin-right"><i class="fa fa-diamond w3-margin-right"></i>Design</button>
+    <button class="w3-button filter w3-margin-right active">ALL</button>
