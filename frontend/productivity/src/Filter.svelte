14273b2c2ff95fdfc09df995662e70f58a3f26a1
rainerknabenbauer <rainer.knabenbauer@posteo.de>
2021-01-13 16:41:09 +0100

    Add Reminder filter

--- frontend/productivity/src/Filter.svelte
+++ frontend/productivity/src/Filter.svelte
@@ -19,4 +19,5 @@
     <button class="w3-button w3-white w3-hide-small filter w3-margin-right"><i class="fa fa-trash-o"></i></button>
     <button class="w3-button w3-white w3-hide-small filter"><i class="fa fa-odnoklassniki w3-margin-right"></i>Recommendations</button>
     <button class="w3-button w3-white filter"><i class="fa fa-heartbeat w3-margin-right"></i>History</button>
-    <button class="w3-button filter">ALL</button>
+    <button class="w3-button w3-white filter"><i class="fa  fa-envelope-o w3-margin-right"></i>Reminder</button>
+    <button class="w3-button filter">Pinboard</button>
