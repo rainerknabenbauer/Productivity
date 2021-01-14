fbf056caf9160d626aff28c4bb79e382dcfc94a7
rainerknabenbauer <rainer.knabenbauer@posteo.de>
2021-01-14 21:35:52 +0100

    Make lines a little less stressful

--- frontend/productivity/src/App.svelte
+++ frontend/productivity/src/App.svelte
@@ -94,8 +94,9 @@
 		
 		tasks.forEach(task => {
 			context.beginPath();
-			context.moveTo(0, 0);
+			context.moveTo(window.innerWidth/2, window.innerHeight/2-wrapper.offsetTop, 0);
 			context.lineTo(task.ui.xposition-wrapper.offsetLeft+175, task.ui.yposition-wrapper.offsetTop+25);
+			context.strokeStyle = '#dddddd';
 			context.stroke();
 			context.closePath();
 		})
