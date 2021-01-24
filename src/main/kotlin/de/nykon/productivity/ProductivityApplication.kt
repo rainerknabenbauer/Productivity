cc476e8880149b8ee64dfb6ce0edaf966ad65b69
rainerknabenbauer <rainer.knabenbauer@posteo.de>
2021-01-24 22:32:22 +0100

    Enable scheduling

--- src/main/kotlin/de/nykon/productivity/ProductivityApplication.kt
+++ src/main/kotlin/de/nykon/productivity/ProductivityApplication.kt
@@ -2,8 +2,10 @@ package de.nykon.productivity
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class ProductivityApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/de/nykon/productivity/utility/Purger.kt
+++ src/main/kotlin/de/nykon/productivity/utility/Purger.kt
@@ -2,7 +2,9 @@ package de.nykon.productivity.utility
 
 import de.nykon.productivity.PurgeService
 import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
 
+@Component
 class Purger(private val purgeService: PurgeService) {
 
     @Scheduled(cron ="5 4 * * *")
