8cb5db4f4a0103808c43f26e93238f74f197ad83
rainerknabenbauer <rainer.knabenbauer@posteo.de>
2021-02-22 16:46:35 +0100

    Remove parent button

--- frontend/productivity/src/LinkTask.svelte
+++ frontend/productivity/src/LinkTask.svelte
@@ -11,6 +11,7 @@
             task.title.toLowerCase().includes(search.toLowerCase()) &&
             !task.isDeleted
         )
+        console.log(suggestions)
     }
 
     function saveTask() {
@@ -24,22 +25,33 @@
         padding: 4px 7px 4px 7px;
         width: 100%;
         resize: none;
+        outline: none;
     }
 
     .selection {
-        width: 100%
+        width: 100%;
+        height: 55%;
+    }
+
+    .unlink {
+        font-size: 8pt;
+        float: right;
+        padding-right: 5px;
+        padding-top: 2px;
     }
 </style>
 
+Search task title:
 <!-- svelte-ignore a11y-autofocus -->
 <textarea class="textarea" autofocus bind:value={search} on:input={createSuggestion}></textarea>
-
+Select parent task: 
+{#if Array.isArray(task.parentTasks) && task.parentTasks.length}
+    <span class="unlink" on:click={() => task.parentTasks = []}>Remove parent</span>
+{/if}
 <select class="selection" multiple bind:value={selection}>
 	{#each suggestions as suggestion (suggestion.id)}
-		<option value={suggestion.id}>
+		<option value={suggestion.id} on:click={() => task.parentTasks = selection}>
 			{suggestion.title}
 		</option>
 	{/each}
 </select>
-
-<button on:click={saveTask}>link</button>
